2fd2eb4d3e0de985fc125937c70f417e076c1b66
tosabyte <29666451+tosabyte@users.noreply.github.com>
2019-03-11 20:07:50 +0700

    check backupninja package

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,8 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_packages(host):
+    p = host.package("backupninja")
+    assert p.is_installed
